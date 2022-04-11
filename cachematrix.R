1d8b105eacad9f11b1e7eca7adac355d08e7eece
Hafid Mammass <hmammass@gmail.com>
2022-04-12 01:09:40 +0200

    second version

--- cachematrix.R
+++ cachematrix.R
@@ -14,13 +14,14 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   ##m1=makeCacheMatrix(x)
   m2<-matrix(nrow = ncol(x), ncol=nrow(x))
-  i<-1
+ 
   j<-1
-  while (j < = ncol(x))
+  while (j <= ncol(x))
   {
+    i<-1
     while (i <= nrow(x) )
     {
-      m2[j,i]=m[i,j]
+      m2[j,i]=x[i,j]
       i<-i+1
     }
     j<-j+1
