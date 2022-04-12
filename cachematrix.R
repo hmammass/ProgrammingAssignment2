0aba63598e88cfe670bf6da861984a2eee3da36e
Hafid Mammass <hmammass@gmail.com>
2022-04-12 02:18:29 +0200

    final version

--- cachematrix.R
+++ cachematrix.R
@@ -4,28 +4,48 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-m<<-x
+  xcache<-NULL
+set<-function(y){
+  x<<-y
+  xcache<<-NULL
+}
+get<-function() {x}
+setcache<-function(y)  {xcache<<-y}
+
+getcache<-function() {xcache}
+
+list(set=set,get=get, setcache=setcache, getcache=getcache)
+
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x,...) {
         ## Return a matrix that is the inverse of 'x'
-  ##m1=makeCacheMatrix(x)
-  m2<-matrix(nrow = ncol(x), ncol=nrow(x))
- 
+  y<-makeCacheMatrix(x)
+  
+  m<-y$getcache()
+  if (!is.null(m))
+  {
+    message('getting cached inverse of matrix')
+    return(m)
+  }
+  data<-y$get()
+  ## this is not inverse just to replace the code of inversion
+  m2<-matrix(nrow=ncol(x),ncol=nrow(x))
   j<-1
-  while (j <= ncol(x))
+  while (j <= ncol(data))
   {
     i<-1
-    while (i <= nrow(x) )
+    while (i <= nrow(data) )
     {
-      m2[j,i]=x[i,j]
+      m2[j,i]=data[i,j]
       i<-i+1
     }
     j<-j+1
   }
     
+  y$setcache(m2)
   m2
 }
